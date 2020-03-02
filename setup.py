0cfe94bc86cae32f7edf723c23ba8f00f862c500
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2020-03-02 13:33:03 +0100

    fix installation + dependencies

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['hsm'],
     package_dir={'': 'src'},
-    scripts=['scripts/viewer.py']
+    packages=['hsm', 'hsm.viewer', 'hsm.viewer.xdot'],
+    package_data={'hsm.viewer': ['img/*']},
 )
 
 setup(**d)
